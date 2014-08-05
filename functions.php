63bf624f7501ae78ed5c8afddb9ecc85fd64df46
Artem Teleshev <artem.teleshev@gmail.com>
2014-08-05 18:10:08 +0300

    Modified view of class methods

--- functions.php
+++ functions.php
@@ -4,8 +4,29 @@
 include '_init.php';
 
 $rf = new ReflectionClass($cb);
-echo 'Properties: ' . PHP_EOL;
-var_dump($rf->getProperties());
-echo 'Methods: ' . PHP_EOL;
-var_dump($rf->getMethods());
+
+//echo 'Properties: ' . PHP_EOL;
+//var_dump($rf->getProperties());
+
+echo "Methods: " . PHP_EOL;
+foreach ($rf->getMethods() as $rfMethod)
+{
+  $parameters = [];
+  foreach ($rfMethod->getParameters() as $rfParameter)
+  {
+    $parameter = trim("{$rfParameter->getClass()} \${$rfParameter->getName()}");
+
+    if ($rfParameter->isDefaultValueAvailable())
+    {
+      $parameter .= " = {$rfParameter->getDefaultValue()}";
+    }
+
+    if ($parameter)
+    {
+      $parameters[] = $parameter;
+    }
+  }
+  
+  echo "{$rfMethod->class}::{$rfMethod->name}(" . implode(', ', $parameters) . ")" . PHP_EOL;
+}
 
